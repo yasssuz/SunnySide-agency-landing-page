58badfc31eccae2d77c4f4805cfcb89040317961
Karim Yassine <karimwebdevelopment@gmail.com>
2021-06-14 22:11:30 +0200

    deploy ready

---
+++ public/assets/index.9176e51b.js
@@ -0,0 +1,1 @@
+const e=document.querySelector(".header__nav-toggler");e.addEventListener("click",(()=>{return t=e,document.querySelector(".header__navbar").classList.toggle("active"),void t.classList.toggle("active");var t}));
